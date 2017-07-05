881858c892976755912816b0c9dd7009170e98ce
atul380 <atulkaushik@gmail.com>
2017-07-05 11:29:51 +0100

    2nd update - start and end
    2nd update - print statement added to the start and end of the code

--- Testcode1.py
+++ Testcode1.py
@@ -1,9 +1,10 @@
 ## This is my first Python code
+print ("Start of code")
 print ("Hello Atul. This is first python code")
 num1 = 100
 num1
 num2 = 20000
 num1 + num2
-print ("Hello")
 print ("Sum of " + str(num1) + " and " + str(num2) + " is: " + str(num1+num2))
 print ("Product of both numbers is " + str(num1*num2))
+print ("End of code")
