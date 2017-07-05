1a4ad6a8fb1be30b121f345e2bacf619e8945d69
atul380 <atulkaushik@gmail.com>
2017-07-05 11:20:20 +0100

    Create Testcode1.py

---
+++ Testcode1.py
@@ -0,0 +1,8 @@
+## This is my first Python code
+print ("Hello Atul. This is first python code")
+num1 = 100
+num1
+num2 = 20000
+num1 + num2
+print ("Hello")
+print ("Sum of " + str(num1) + " and " + str(num2) + " is: " + str(num1+num2))
